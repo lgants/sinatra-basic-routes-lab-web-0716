063c354a47fb8079a8136dc131522bab6eeef2b5
lgants <lgants@gmail.com>
2016-08-13 19:37:38 -0400

    sinatra basic routes lab complete -ltg

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+  get '/name' do
+    "My name is Logan"
+  end
+
+  get '/hometown' do
+    "My hometown is Madison, WI"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'That's Life'"
+  end
+
+
 end
